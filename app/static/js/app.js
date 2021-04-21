9fdd3eaee4514fa402f741c48bc622941409b242
Deandrew007 <deamoore07@gmail.com>
2021-04-21 02:30:15 -0500

    added homepage and register

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,34 +1,144 @@
 /* Add your Application JavaScript */
+const Home = {
+  name: 'Home',
+  template: `
+  <div class="home-grid">
+  <div class="home-text-section">
+      <h1>Buy and Sell Cars Online</h1>
+      <p>United Auto Sales provides the fastest easiest and most user friendly way to buy and sell cars online.
+      Find a Great Price on the Vehicle You Want.</p>
+    
+      <button class="btn btn-primary mb-2" @click="$router.push('register')">Register</button>
+      
+      <button class="btn btn-success mb-2"  @click="$router.push('login')" >Login</button>
+     
+  </div>
+
+  <img class="home-image" src="../static/images/car.png" alt="Car Image">
+
+</div>
+  `,
+  data() {
+  return {
+
+  }
+  }
+ };
+
+ const Register = {
+  name: 'Register',
+  template: `
+  <div class="jumbotron">
+  <div class="register-grid">
+  <h1>Register New User</h1>
+  <form id="registerUser" enctype="multipart/form-data">
+          <div class = "form-grid">
+          <div>
+              <label for="username" class="form-label">Username</label>
+              <br>
+              <input type="text" name="username" id="username" required>
+          </div>
+          <div>
+              <label for="password" class="form-label">Password</label>
+              <br>
+              <input type="password" name="password" id="password" required>
+          </div>
+          </div>
+          <div class = "form-grid">
+              <div>
+                  <label for="fullname" class="form-label">Fullname</label>
+                  <br>
+                  <input type="text" name="fullname" id="fullname" required>        
+              </div>
+              <div>
+                  <label for="email" class="form-label">Email</label>
+                  <br>
+                  <input type="email" name="email" id="email" required>        
+              </div>
+              </div>
+          <label for="location" class="form-label">Location</label>
+          <br>
+          <input type="text" name="location" id="location" required>
+          <br>
+          <label for="Biography" class="form-label">Biography</label>
+          <br>
+          <textarea class="form-control" name="biography"></textarea>
+          <br>
+          <label for="photo" class="form-label">Upload Photo</label>
+          <br>
+          <input type="file" name="photo" class="form-control">
+          <br>
+          <button type="submit" class="btn btn-success">Register</button>
+  </form>
+  
+  </div>
+</div>
+  `,
+  data() {
+  return {
+
+  }
+  }
+ };
+
+ const Login = {
+  name: 'Login',
+  template: `
+  <div class="jumbotron">
+    <div class="login-grid">
+
+    </div>
+</div>
+  `,
+  data() {
+  return {
+
+  }
+  }
+ };
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+  { path: '/', component: Home },
+  { path: '/register', component: Register },
+  { path: '/login', component: Login },
+
+
+  ]
+ });
+
 const app = Vue.createApp({
   data() {
     return {
-      welcome: 'Hello World! Welcome to VueJS'
     }
-  }
+  },
+  components: {
+    'home': Home,
+    }
 });
 
 app.component('app-header', {
   name: 'AppHeader',
   template: `
-      <header>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-            <a class="navbar-brand" href="#">VueJS App</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-      </header>    
+  <header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <a class="navbar-brand" href="/">United Auto Sales</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link to="/register" class="nav-link">Register</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">Login</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>    
   `,
   data: function() {
     return {};
@@ -51,4 +161,5 @@ app.component('app-footer', {
   }
 })
 
+app.use(router)
 app.mount('#app');
