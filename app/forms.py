d3f41a3b2d90d89e5d326f02a86d50ed8b79528f
Abygayle Ivey <abyivey@gmail.com>
2021-04-24 15:59:20 -0500

    form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms import StringField, TextAreaField, SelectField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed, FileField, FileRequired
 
-
+class AddForm(FlaskForm):
+    make = StringField('make', validators=[DataRequired()])
+    model= StringField('model')
+    colour= StringField('colour')
+    year = StringField('year', validators=[DataRequired(), year()])
+    price = StringField('price', validators=[DataRequired()])
+    car_type = SelectField('Type of Car', choices=[("SUV","SUV"),("Sedan","Sedan"),("Hatchback","Hatchback"),("Minivan","Minivan")])
+    transmission = SelectField('Transmissions', choices=[("Automatic","Automatic"),("Manual","Manual")])
+    description = TextAreaField('description', validators=[DataRequired()])
+    photo = FileField('Profile Photo', validators=[FileRequired(), FileAllowed(["jpg","png"],"Images only!")
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -34,6 +34,88 @@ app.component('app-header', {
     return {};
   }
 });
+const addVehicle = Vue.component('addVehicle', {
+    template: `
+    <div class="car-card">
+
+    <div>
+        <img class="single" src="{{ url_for('get_image', filename = car.photo)}} "/>
+    </div>
+    <div class="single-card-body">
+        <h2>{{ car.title }}</h2>
+        <div class="attributes">
+            <span class="attribute price">{{ car.price }}</span> <span class="attribute type">{{ car.typeHA}}</span>
+        </div>
+        <p>{{ car.description }}</p>
+        <div class="add-tab">
+            <p><i class="fa fa-make"></i> {{ car.make }} Make</p>  
+            <p><i class="fa fa-model"></i> {{ car.model}} Model</p>
+        </div>
+
+        <div class="add-tab">
+            <p><i class="fa fa-colour"></i> {{ car.colour }} Colour</p>  
+            <p><i class="fa fa-year"></i> {{ car.year}} Year </p>
+        </div>
+
+        <div class="add-tab">
+            <p><i class="fa fa-price"></i> {{ car.price}} Price</p>  
+            <p><i class="fa fa-car_type"></i> {{ car.car_type}} Car Type</p>
+        </div>
+	
+        <p><i class="fa fa-transmission" aria-hidden="true"></i> {{ car.transmission }}</p>
+        <p><i class="fa fa-description" aria-hidden="true"></i> {{ car.description }}</p>
+
+        <button type="submit" class="btn email-btn">Email</button>
+    </div>
+
+</div>
+
+
+    `,
+    methods: {
+        uploadpost: function() {
+            self = this;
+            let AddForm = document.getElementById('AddForm');
+            let form_data = new FormData(AddForm);
+            let user_id = localStorage.getItem('car_id');
+            fetch("/api/users/" + car_id + "/Add Vehicle", {
+
+                    method: 'POST',
+                    body: form_data,
+                    headers: {
+                        'X-CSRFToken': token,
+                        'Authorization': 'Bearer ' + localStorage.getItem('token')
+                    },
+                    credentials: 'same-origin'
+                })
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(jsonResponse) {
+                    // display a success message
+                    //undefined - no erros
+                    console.log(jsonResponse.message)
+                    if (jsonResponse.message == "Car was uploaded sucessfully") {
+                        router.push('/search');
+                    }
+
+
+
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
+    },
+    data: function() {
+        return {
+            id: 0,
+            messages: ""
+
+
+        }
+    }
+});
 
 app.component('app-footer', {
   name: 'AppFooter',
