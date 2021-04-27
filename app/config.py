f0953b7fd8fa3656fa3b85dde158dd1956d7fbaf
Deandrew007 <deamoore07@gmail.com>
2021-04-27 01:46:48 -0500

    added new heroku config

--- app/config.py
+++ app/config.py
@@ -4,7 +4,11 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     # UPLOAD_FOLDER= './uploads'
     UPLOAD_FOLDER = './app/static/uploads'
